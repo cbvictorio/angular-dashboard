d2de85e078a64ac539504cc02d59976a81d82e15
Cesar <cbvictorio@gmail.com>
2026-02-03 23:27:28 -0800

    add initial theming logic with angular signals

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from "@angular/router";
+import { ThemeService } from '../services/theme';
 
 interface NavLink {
   path: string;
@@ -14,10 +15,23 @@ interface NavLink {
   styleUrl: './navbar.css',
 })
 
-export class Navbar {
+export class Navbar implements OnInit {
+  
+  themeService = inject(ThemeService);
+  isDark = this.themeService.isDarkMode;
+
   navLinks: NavLink[] = [
     { path: '/', label: 'Home' },
     { path: '/monitor', label: 'System Monitor' },
     { path: '/settings', label: 'Settings' }
   ];
+
+  onToggleClick() {
+    this.themeService.toggleTheme();
+  }
+
+  ngOnInit(): void {
+    console.log(`is dark mode enabled: ${this.isDark()}`)
+  }
+
 }
